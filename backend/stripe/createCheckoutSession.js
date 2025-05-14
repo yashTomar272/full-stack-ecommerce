919bf9d7cac1c38a99d77f5c325f7938a54715d1
yashTomar272 <tomarji272@gmail.com>
2025-05-15 01:44:36 +0530

    change name

--- backend/stripe/createCheckoutSession.js
+++ backend/stripe/createCheckoutSession.js
@@ -1,5 +1,6 @@
 const router=require("express").Router();
 const Stripe = require("stripe");
+ const URL = process.env.WEB_URL;
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -19,13 +20,13 @@ router.post("/create-checkout-session", async (req, res) => {
       },
       quantity: 1,
     }));
-    const successUrl = `https://full-stack-ecommerce-kappa-eight.vercel.app/success?cartItems=${encodeURIComponent(JSON.stringify(cartItems))}`;
+    const successUrl = `${WEB_URL}/success?cartItems=${encodeURIComponent(JSON.stringify(cartItems))}`;
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items,
       mode: "payment",
       success_url: successUrl,
-      cancel_url: "https://full-stack-ecommerce-kappa-eight.vercel.app/cancel",
+      cancel_url:`${WEB_URL}/cancel`
     });
   
     res.json({
