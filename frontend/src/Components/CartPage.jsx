0eae408fb8f6d7a4ec05e379b1ae1c263d965b0b
yashTomar272 <tomarji272@gmail.com>
2025-05-19 01:01:04 +0530

    change final

--- frontend/src/Components/CartPage.jsx
+++ frontend/src/Components/CartPage.jsx
@@ -74,7 +74,7 @@ const PlaceOrder = async () => {
   try {
    
 
-    const res = await fetch("https://full-stack-ecommerce-daos.vercel.app/create-checkout-session", {
+    const res = await fetch("https://full-stack-ecommerce-green.vercel.app/create-checkout-session", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
